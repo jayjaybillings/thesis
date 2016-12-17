2f4b3ea43779ff4248526c19017b207050a8cd4b
Jay Jay Billings <billingsjj@ornl.gov>
2016-12-17 16:18:25 -0500

    Got the vaadin sample app working with OSGi.
    Committing that shit!
    
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

---
+++ myapplication/src/main/java/com/example/myapplication/MyUI.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    private HttpService httpService;
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+    
+	@Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener( e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+    
+    /**
+     * OSGi bundle activator with annotation instead of activator class.
+     * @param context
+     */
+    @Activate
+	public void start(BundleContext context) {
+		System.out.println("VAADIN bundle started.");
+		try {
+			httpService.registerServlet("/", new MyUIServlet(), null, null);
+		} catch (ServletException | NamespaceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+    
+    public void setService(HttpService httpService) {
+    	this.httpService = httpService;
+    }
+    
+}
